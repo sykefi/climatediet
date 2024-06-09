095d768ea97a62f0d8e70ee573a0bfe96fde1cc2
Simo <simo.paasisalo@syke.fi>
2024-06-09 05:44:13 +0000

    linted

--- src/globals.ts
+++ src/globals.ts
@@ -1,6 +1,8 @@
 export default {
   install(app) {
-    const componentFiles = import.meta.glob('./components/forms/*.vue', {eager: true})
+    const componentFiles = import.meta.glob('./components/forms/*.vue', {
+      eager: true,
+    })
 
     Object.entries(componentFiles).forEach(([path, m]) => {
       const componentName = path
