dd9789b61e8fc32782b92eb4efc166c59912f3c1
Koskelainen Sara <Sara.Koskelainen@syke.fi>
2026-03-17 09:59:58 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,10 @@ const eslintPluginPrettierRecommended = await import(
   'eslint-plugin-prettier/recommended'
 )
 import globals from 'globals'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfigWithVueTs(
   vueTsConfigs.recommended,
